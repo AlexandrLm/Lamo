21e06f0c4df785065749ad98abce42f9540af0d0
Alex <vikal1709@gmail.com>
2026-06-27 21:42:25 +0900

    Initial Commit

---
+++ Lamo/LamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LamoApp.swift
+//  Lamo
+//
+//  Created by Aleksandr on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct LamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
